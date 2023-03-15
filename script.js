9b68d9997934cdca6dfd388c7bf0ed2007cf32c6
Lane Enget <enget006@umn.edu>
2023-03-14 19:30:54 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('testing');
+
+// Unsplash API
+const count = 10;
+const apiKey = '3328a6a54d08026bf3f10f849af885ba'
+const apiUrl = `https:://flicker.photos.getPopular/?api_key=${apiKey}&per_page=${count}`;
+
+
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);    
+    } catch (error) {
+        // catch error
+    }
+}
+
+// On Load
+getPhotos();
